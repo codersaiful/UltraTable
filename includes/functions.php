ab0943444b9a419389d4fea055c8282eeaed5d7c
Saiful Islam <codersaiful@gmail.com>
2020-08-11 19:18:38 +0600

    Pagination Work is completed with style

--- includes/functions.php
+++ includes/functions.php
@@ -9,12 +9,13 @@ if( !function_exists( 'ultratable_pagination' ) ){
      * @return string
      */
     function ultratable_pagination( $args, $datas, $atts, $POST_ID, $product_loop ){
+        echo wp_kses_post( '<div class="ultratable-pagination-wrapper" >' );
             $big = 99999999;
             $paginate = paginate_links( array(
                 //'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
                 //'format' => apply_filters( 'wpto_pagination_format', '?paged=%#%', $args ),
                 'mid_size'  => apply_filters( 'ultratable_pgn_mid_size', 3, $args, $datas, $atts, $POST_ID, $product_loop ),
-                'prev_next' =>  false,
+                'prev_next' => apply_filters( 'ultratable_next_prev_show', true, $args, $datas, $atts, $POST_ID, $product_loop ),
                 'current' => max( 1, $args['paged'] ),
                 'total' => $product_loop->max_num_pages,
                 'type'  => apply_filters( 'ultratable_pgn_type', 'list', $args, $datas, $atts, $POST_ID, $product_loop ),
@@ -22,7 +23,7 @@ if( !function_exists( 'ultratable_pagination' ) ){
 		//'after_page_number'  => apply_filters( 'ultratable_pgn_type', '', $args, $datas, $atts, $POST_ID, $product_loop ),
             ));
             echo $paginate;
-            
+        echo wp_kses_post( '</div>' );    
         /**
         $total   = isset( $args['total'] ) ? intval( $args['total'] ) : 2;//isset( $wp_query->max_num_pages ) ? $wp_query->max_num_pages : 1;
 	$current = get_query_var( 'paged' ) ? intval( get_query_var( 'paged' ) ) : 1;
@@ -45,4 +46,4 @@ if( !function_exists( 'ultratable_pagination' ) ){
     }
 }
 add_action( 'ultratable_footer', 'ultratable_pagination', 10, 5 );
-add_action( 'ultratable_header', 'ultratable_pagination', 10, 5 );
+//add_action( 'ultratable_header', 'ultratable_pagination', 10, 5 );
